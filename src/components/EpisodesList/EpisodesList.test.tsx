b581dd111ee9b348b286300023299dacc8a1933d
Galtsev Roman <grsjob@yandex.ru>
2022-06-15 21:22:38 +0300

    added EpisodesList.test

--- src/components/EpisodesList/EpisodesList.test.tsx
+++ src/components/EpisodesList/EpisodesList.test.tsx
@@ -1,13 +1,17 @@
 import {act, render} from "@testing-library/react";
 import {store} from "../../state/store";
-import {deleteEpisode, initializeList, loadBBEpisodes} from "./EpisodesListSlice";
-import DataService from "../../services/DataService";
-import Header from "../Header/Header";
-import App from "../App/App";
+import {
+    deleteEpisode,
+    loadBBEpisodes,
+    addCharacter,
+    deleteCharacter,
+    sortByEpisodeNumber,
+    sortByCharactersDown,
+    sortByCharactersUp} from "./EpisodesListSlice";
 
 
 describe('EpisodesListSlice testing', function () {
-    const episodes = [
+    const ep = [
         {
             air_date: '01-20-2008',
             characters: ['Walter White', 'Jesse Pinkman'],
@@ -19,8 +23,8 @@ describe('EpisodesListSlice testing', function () {
         },
         {
             air_date: '01-20-2008',
-            characters: ['Walter White', 'Jesse Pinkman'],
-            episode: "1",
+            characters: ['Walter White', 'Jesse Pinkman', 'Jesse Pinkman'],
+            episode: "2",
             episode_id: 2,
             season: '1',
             series: "Breaking Bad",
@@ -29,7 +33,7 @@ describe('EpisodesListSlice testing', function () {
         {
             air_date: '01-20-2008',
             characters: ['Walter White', 'Jesse Pinkman'],
-            episode: "1",
+            episode: "3",
             episode_id: 3,
             season: '1',
             series: "Bad",
@@ -41,18 +45,102 @@ describe('EpisodesListSlice testing', function () {
 
     beforeEach(async () => {
         await act(async ()=>{
-            store.dispatch(loadBBEpisodes(episodes))
+            store.dispatch(loadBBEpisodes(ep))
         })
     })
-    it('should be load BB Episodes', async () => {
-        const episodesArray = store.getState().list.episodes
-        expect(episodes.length === 3).toBeTruthy()
+    describe('download testing', function () {
+        it('should be load BB Episodes', async () => {
+            const episodesArray = store.getState().list.episodes
+            expect(episodesArray.length > 0).toBeTruthy()
+        });
+        it('should be load only BB Episodes', async () => {
+            const episodesArray = store.getState().list.episodes
+            const arrayOfEpisodesOfAnotherSeries = episodesArray.filter(episode => episode.series !== 'Breaking Bad')
+            expect(episodesArray.length === 2).toBeTruthy()
+            expect(arrayOfEpisodesOfAnotherSeries.length === 0).toBeTruthy()
+        });
     });
-    it('should be remove Episode from the episodes array ', function () {
-        const  episodesArrayBefore = store.getState().list.episodes
-        console.log(episodesArrayBefore)
-        store.dispatch(deleteEpisode(1))
-        const episodesArrayAfter = store.getState().list.episodes
-        expect(episodesArrayAfter.length === 2).toBe(2)
+    describe('remove episode testing', function () {
+        it('should be remove Episode from the episodes array ', function () {
+            store.dispatch(deleteEpisode(1))
+            const episodesArrayAfterDelete = store.getState().list.episodes
+            expect(episodesArrayAfterDelete.length).toBe(1)
+        });
+        it('should be remove all episodes ', function () {
+            const episodesArrayBeforeRemoveAllEpisodes = store.getState().list.episodes
+            for (let i = 1; i <= episodesArrayBeforeRemoveAllEpisodes.length; i++) {
+                store.dispatch(deleteEpisode(i))
+            }
+            const episodesArrayAfterRemoveAllEpisodes = store.getState().list.episodes
+            expect(episodesArrayAfterRemoveAllEpisodes.length).toBe(0)
+        });
+        it('should be impossible to delete an episode with an invalid id ', function () {
+            const episodesArrayBeforeRemoveAllEpisodes = store.getState().list.episodes
+            store.dispatch(deleteEpisode(8))
+            const episodesArrayAfterRemoveAllEpisodes = store.getState().list.episodes
+            expect(episodesArrayAfterRemoveAllEpisodes.length).toBe(episodesArrayBeforeRemoveAllEpisodes.length)
+        });
+    });
+    describe('testing adding an actor', function () {
+        it('should be add an actor to the episode ', function () {
+            const episodesArrayBeforeAddingAnActor = store.getState().list.episodes
+            const firsEpisodeBeforeAdding = episodesArrayBeforeAddingAnActor.find(episode => episode.episode_id === 1)
+            store.dispatch(addCharacter(1))
+            const episodesArrayAfterAddingAnActor = store.getState().list.episodes
+            const firsEpisodeAfterAdding = episodesArrayAfterAddingAnActor.find(episode => episode.episode_id === 1)
+            expect(firsEpisodeAfterAdding!.characters.length >firsEpisodeBeforeAdding!.characters.length).toBeTruthy()
+        });
+        it('should be impossible to add an actor to the episode, if id is invalid ', function () {
+            const episodes = store.getState().list.episodes
+            const idMap = new Map()
+            for (let i = 0; i < episodes.length; i++) {
+                idMap.set(episodes[i].episode_id, episodes[i]!.characters.length)
+            }
+            const invalidId = 9
+            const valuesBefore = idMap.values()
+            store.dispatch(addCharacter(invalidId))
+            const valuesAfter = idMap.values()
+            expect(idMap.has(invalidId)).toBeFalsy()
+            expect(JSON.stringify(valuesBefore)===JSON.stringify(valuesAfter)).toBeTruthy()
+        });
+    });
+    describe('testing removing an actor', function () {
+        it('should be remove an actor to the episode', function () {
+            const episodesArrayBeforeRemoveAnActor = store.getState().list.episodes
+            const firsEpisodeBeforeRemoving = episodesArrayBeforeRemoveAnActor.find(episode => episode.episode_id === 1)
+            store.dispatch(deleteCharacter(1))
+            const episodesArrayAfterRemoveAnActor = store.getState().list.episodes
+            const firsEpisodeAfterRemoving = episodesArrayAfterRemoveAnActor.find(episode => episode.episode_id === 1)
+            expect(firsEpisodeAfterRemoving!.characters.length < firsEpisodeBeforeRemoving!.characters.length).toBeTruthy()
+        });
+        it('t should be impossible to make the number of actors in an episode < 0 ', function () {
+            store.dispatch(deleteCharacter(1))
+            store.dispatch(deleteCharacter(1))
+            store.dispatch(deleteCharacter(1))
+            const episodesArrayAfterRemoveAnActor = store.getState().list.episodes
+            const firsEpisodeAfterRemoving = episodesArrayAfterRemoveAnActor.find(episode => episode.episode_id === 1)
+            expect(firsEpisodeAfterRemoving!.characters.length < 0).toBeFalsy()
+        });
+    });
+    describe('episodes sorting testing ', function () {
+        it('should be sorted in ascending episode number', function () {
+            const episodes = store.getState().list.episodes
+            store.dispatch(sortByEpisodeNumber())
+            const index = 0
+            expect(Number(episodes[index].episode) < Number(episodes[index+1].episode)).toBeTruthy()
+            expect(Number(episodes[index].episode) > Number(episodes[index+1].episode)).toBeFalsy()
+        });
+        it('should be sorted in descending order by the number of actors in the episode', function () {
+            store.dispatch(sortByCharactersDown())
+            const index = 0
+            const episodes = store.getState().list.episodes
+            expect(episodes[index].characters.length > episodes[index+1].characters.length ).toBeTruthy()
+        });
+        it('should be sorted in ascending order by the number of actors in the episode', function () {
+            store.dispatch(sortByCharactersUp())
+            const index = 0
+            const episodes = store.getState().list.episodes
+            expect(episodes[index].characters.length < episodes[index+1].characters.length ).toBeTruthy()
+        });
     });
 });
